d509756edc239632872be04d7612ac72cbd621ec
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-25 13:22:01 +0800

    使用LifeCycles重构代码

--- app/src/main/java/com/winnerwinter/lifecycle/MainActivity.java
+++ app/src/main/java/com/winnerwinter/lifecycle/MainActivity.java
@@ -3,34 +3,14 @@ package com.winnerwinter.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.widget.Chronometer;
-
 
 public class MainActivity extends AppCompatActivity {
-
-    Chronometer chronometer;
-    private long elapsedTime;
-
+    MyChronometer chronometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chronometer = findViewById(R.id.meter);
-        // chronometer.setBase(SystemClock.elapsedRealtime());
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        elapsedTime = SystemClock.elapsedRealtime() - chronometer.getBase();
-        chronometer.stop();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        chronometer.setBase(SystemClock.elapsedRealtime() - elapsedTime);
-        chronometer.start();
+        getLifecycle().addObserver(chronometer);
     }
 }
